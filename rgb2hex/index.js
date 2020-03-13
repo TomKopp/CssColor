d2f3961710ab93f95773ed7b88f70bcd48c1fc67
Tom Kopp <witzbould@outlook.com>
2020-03-13 23:18:52 +0100

    use utility functions

--- rgb2hex/index.js
+++ rgb2hex/index.js
@@ -1,11 +1,14 @@
-const uByte2hex = (number) => {
-	const val = Math.abs(Number(number));
-	const uByte = 256;
-	const baseHex = 16;
-
-	return val < uByte ? val.toString(baseHex) : 0;
-};
+import { mapValueFromInputToOutputRange, uByte2hex } from '@witzbould/utils';
 
 const uByte2hexTwoDigit = (number) => uByte2hex(number).padStart(2, '0');
 
-export const rgb2hex = (red, green, blue, alpha) => `#${uByte2hexTwoDigit(red)}${uByte2hexTwoDigit(green)}${uByte2hexTwoDigit(blue)}${alpha ? uByte2hexTwoDigit(alpha) : ''}`;
+/**
+ * Convert rgb(a) values to hex(a)
+ *
+ * @param  {number} red The red color part
+ * @param  {number} green The green color part
+ * @param  {number} blue The blue color part
+ * @param  {number} [alpha] The opacity between 0 and 1
+ * @return {string} The hex value of the color in the form of #RRGGBBAA
+ */
+export const rgb2hex = (red, green, blue, alpha) => `#${uByte2hexTwoDigit(red)}${uByte2hexTwoDigit(green)}${uByte2hexTwoDigit(blue)}${alpha ? uByte2hexTwoDigit(mapValueFromInputToOutputRange(alpha, 0, 1, 0, 255)) : ''}`;
