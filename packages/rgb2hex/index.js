93a8b6a22fa0e42ba99ef76b2580ddd9f3a208f5
Tom Kopp <witzbould@outlook.com>
2020-03-21 16:16:51 +0100

    use updated dependencies

--- packages/rgb2hex/index.js
+++ packages/rgb2hex/index.js
@@ -1,4 +1,5 @@
-import { mapValueFromInputToOutputRange, uByte2hex } from '@witzbould/utils';
+import { mapValueRange } from '@witzbould/utils-map-value-range';
+import { uByte2hex } from '@witzbould/utils-u-byte2hex';
 
 const uByte2hexTwoDigit = (number) => uByte2hex(number).padStart(2, '0');
 
@@ -11,4 +12,4 @@ const uByte2hexTwoDigit = (number) => uByte2hex(number).padStart(2, '0');
  * @param  {number} [alpha] The opacity between 0 and 1
  * @return {string} The hex value of the color in the form of #RRGGBBAA
  */
-export const rgb2hex = (red, green, blue, alpha) => `#${uByte2hexTwoDigit(red)}${uByte2hexTwoDigit(green)}${uByte2hexTwoDigit(blue)}${alpha ? uByte2hexTwoDigit(mapValueFromInputToOutputRange(alpha, 0, 1, 0, 255)) : ''}`;
+export const rgb2hex = (red, green, blue, alpha) => `#${uByte2hexTwoDigit(red)}${uByte2hexTwoDigit(green)}${uByte2hexTwoDigit(blue)}${alpha ? uByte2hexTwoDigit(mapValueRange(alpha, 0, 1, 0, 255)) : ''}`;
