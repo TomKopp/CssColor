d456007dc3d1179774387d61432dff829df4262b
Tom Kopp <tom.kopp@zalari.de>
2024-09-24 15:22:26 +0200

    build: Update eslint and eslint-plugin versions
    - Update eslint version from 8.56.0 to 9.11.1
    - Update eslint-plugin-import-x version from 4.3.0 to the latest version
    - Update eslint-plugin-jsdoc version from 50.2.4 to the latest version
    - Update eslint-plugin-mocha version from 10.5.0 to the latest version

--- eslint.config.js
+++ eslint.config.js
@@ -1,32 +1,48 @@
 import js from '@eslint/js';
+import eslintPluginImportX from 'eslint-plugin-import-x';
 import jsdoc from 'eslint-plugin-jsdoc';
 import mocha from 'eslint-plugin-mocha';
+import globals from 'globals';
 
 export default [
-  js.configs.recommended,
-  jsdoc.configs['flat/recommended'],
-  {
-    files: ['**/*.js'],
-    plugins: {
-      jsdoc: jsdoc
-    },
-    rules: {
-      'jsdoc/tag-lines': ['error' | 'warn', 'any', { 'startLines': 1 }]
-    }
-  },
-  {
-    files: ['**/test/*.js'],
-    plugins: {
-      mocha
-    },
-    languageOptions: {
-      globals: {
-        describe: 'readonly',
-        it: 'readonly'
-      }
-    },
-    rules: {
-      ...mocha.configs['recommended'].rules
-    }
-  }
+	js.configs.recommended,
+	eslintPluginImportX.flatConfigs.recommended,
+	jsdoc.configs['flat/recommended'],
+	{
+		languageOptions: {
+			ecmaVersion: 2022,
+			sourceType: 'module',
+			globals: {
+				...globals.browser,
+				...globals.node,
+			},
+			parserOptions: {
+				ecmaVersion: 2022,
+				sourceType: 'module',
+			},
+		},
+	},
+	{
+		files: ['**/*.js'],
+		plugins: {
+			jsdoc: jsdoc
+		},
+		rules: {
+			'jsdoc/tag-lines': ['error' | 'warn', 'any', { 'startLines': 1 }]
+		}
+	},
+	{
+		files: ['**/test/*.js'],
+		plugins: {
+			mocha
+		},
+		languageOptions: {
+			globals: {
+				...globals.mocha
+			}
+		},
+		rules: {
+			...mocha.configs['recommended'].rules
+		}
+	}
 ];
